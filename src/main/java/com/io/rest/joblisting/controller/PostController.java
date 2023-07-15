4296598d4405cf602d6d427190c88226300dd177
Niloy Chakravorty <chakravortyniloy@gmail.com>
2023-07-15 22:55:47 +0530

    commit

--- src/main/java/com/io/rest/joblisting/controller/PostController.java
+++ src/main/java/com/io/rest/joblisting/controller/PostController.java
@@ -1,14 +1,15 @@
 package com.io.rest.joblisting.controller;
 
+import com.io.rest.joblisting.model.Post;
 import com.io.rest.joblisting.service.PostServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -27,4 +28,33 @@ public class PostController {
         response.sendRedirect("/swagger-ui.html");
     }
 
+    @GetMapping("/posts")
+    public List<Post> getAll()
+    {
+        return postService.getAll();
+    }
+
+    @GetMapping("/post/{id}")
+    public Optional<Post> getAll(@PathVariable String id)
+    {
+        return postService.getById(id);
+    }
+
+    @PostMapping("post")
+    public Post addPost(@RequestBody Post post)
+    {
+        return postService.newPost(post);
+    }
+
+    @PatchMapping("post/{id}")
+    public Post updatePost(@RequestBody Post post, @PathVariable String id)
+    {
+        return postService.updatePostById(post, id);
+    }
+
+    @DeleteMapping("/post/{id}")
+    public String deletePost(@PathVariable String id)
+    {
+       return  postService.deletePostById(id);
+    }
 }
