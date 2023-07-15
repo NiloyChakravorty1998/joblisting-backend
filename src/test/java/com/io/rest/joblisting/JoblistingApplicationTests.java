fcfa3765b0edb695e90fc10664a5dfe719f95cc1
Niloy Chakravorty <chakravortyniloy@gmail.com>
2023-07-15 17:37:12 +0530

    initial commit

---
+++ src/test/java/com/io/rest/joblisting/JoblistingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.rest.joblisting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoblistingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
